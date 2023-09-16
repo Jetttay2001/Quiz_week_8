64a707f93ad6b39191383dd12c117b1c16ee7c5e
Jett Taylor <you@example.com>
2023-09-16 20:24:10 +1000

    Python code added to Q2

--- climate_q2.py
+++ climate_q2.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# create lists for data
 years = []
 co2 = []
 temp = []
 
+# get the data from the csv
+data = pd.read_csv("climate.csv")
+
+# Append the data
+for index, row in data.iterrows():
+
+    years.append(row["Year"])
+    co2.append(row["CO2"])
+    temp.append(row["Temperature"])
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
+plt.plot(years, co2, 'r*-')
 plt.title("Climate Data") 
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
