6ba2c0fcc8d2d12cff6eb1da9f2b47ce45ead857
Jett Taylor <you@example.com>
2023-09-16 19:50:58 +1000

    Python added to display graphs

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
+import sqlite3
         
 years = []
 co2 = []
 temp = []
+
+# STart using the DB
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+# Taking the data from the database
+cursor.execute("SELECT year, co2, temperature FROM ClimateData")
+
+# Iterate through the results and populate the lists
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+# End of use for the database
+conn.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
